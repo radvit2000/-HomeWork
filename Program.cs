b03e4e2c9fea78bff2501c02a19262bbb2c7da6e
vitaly <2004radvit@gmail.com>
2022-08-22 20:25:08 +0300

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// задача 2
+// Console.WriteLine("введите число");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("введите число");
+// int b = int.Parse(Console.ReadLine());
+// if(a > b)    Console.WriteLine("Первое число больше");
+
+// else    Console.WriteLine("Второе число больше");
+
+// задача 4
+// Console.WriteLine("введите число");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("введите число");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine("введите число");
+// int c = int.Parse(Console.ReadLine());
+
+// int max = a;
+// if (b > max)    max = b;
+// if (c > max)    max = c;
+
+// Console.WriteLine(max);
+
+// Задача 6
+// Console.WriteLine("введите число");
+// int a = int.Parse(Console.ReadLine());
+// if (a % 2 == 0)    Console.WriteLine("да");
+// else    Console.WriteLine("нет");
+
+// Задача 8
+Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+while (b < a + 1){
+    Console.WriteLine(b);
+    b += 2;
+}
